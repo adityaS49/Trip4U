cffb9847180f942821c42f06c8d618c7caff3dd6
Samrat Patel <patel.samrat2000@gmail.com>
2024-03-06 20:50:50 +0530

    two destinations

--- src/components/TopDestinations/TopDestinations.js
+++ src/components/TopDestinations/TopDestinations.js
@@ -1,10 +1,43 @@
-import React from 'react'
-import styles from "./TopDestinations.module.css"
+"use client";
+import { useState } from "react";
+import styles from "./TopDestinations.module.css";
 
 const TopDestinations = () => {
-  return (
-    <div>TopDestinations</div>
-  )
-}
+  const [destinations, setDestionations] = useState([
+    { destination: "DUBAI", color: "#602F70" },
+    { destination: "MALDIVES", color: "#075962" },
+    { destination: "BALI", color: "#2A6B97" },
+    { destination: "SINGAPORE", color: "#002865" },
+    { destination: "KASHMIR", color: "#9B321F" },
+    { destination: "LAKSHADWEEP", color: "#3D4438" },
+    { destination: "AGRA", color: "#655850" },
+    { destination: "AUSTRALIA", color: "#77441F" },
+  ]);
 
-export default TopDestinations
+  return (
+    <div className={styles.container}>
+      <div className={styles.heading_container}></div>
+      <div className={styles.heading_text}>Top Tourist Places</div>
+      <div className={styles.destinations_container}>
+        {destinations.map((item, index) => {
+          return (
+            <div key={index} className={styles.destinations_card}>
+              <img src="/top_destinations_1.png" />
+              <button
+                className={styles.destinations_button}
+                style={{
+                  backgroundColor: item.color,
+                }}
+              >
+                <span>VISIT {item.destination}</span>
+                <img src="/chevron_right.svg" />
+              </button>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default TopDestinations;
