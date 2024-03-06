a3d2a6ae77d2bce50f6c1e52e8ceaaa64075c0f8
Samrat Patel <patel.samrat2000@gmail.com>
2024-03-06 15:23:41 +0530

    margin on each element

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,6 @@ import styles from "./page.module.css";
 import Banner from "@/components/Banner/Banner";
 import AdStrip from "@/components/AdStrip/AdStrip";
 import Carousel from "@/components/Crousel/Crousel";
-import TwoDestination from "@/components/TwoDestination/TwoDestination";
 
 const Home = (props) => {
   return (
@@ -11,7 +10,6 @@ const Home = (props) => {
       <Banner/>
       <AdStrip/>
       <Carousel/>
-      <TwoDestination/>
       <DownloadApp/>
     </main>
   );
